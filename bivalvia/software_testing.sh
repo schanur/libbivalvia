48f2e92ff709c0d6311a88dbc97fa990dc1db106
Bjoern Griebenow <b.griebenow@web.de>
2017-01-31 19:28:34 +0100

    Add incomplete "software_testing" module

---
+++ bivalvia/software_testing.sh
@@ -0,0 +1,11 @@
+BIVALVIA_PATH="${0}"
+
+
+function run_desc_tests_from_function {
+    local MODULE_FILENAME=${1}
+    local FUNCTION_NAME=${2}
+}
+
+function run_desc_tests_from_module {
+    local MODULE_FILENAME=${1}
+}

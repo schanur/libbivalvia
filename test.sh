5adb5d3dc250f4c95a6b1111f888816d710f6f4b
Bjoern Griebenow <b.griebenow@web.de>
2017-02-11 16:38:02 +0100

    test.sh runs all unit test script files

--- test.sh
+++ test.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-for TEST_SCRIPT in $(find -name); do
+BIVALVIA_PATH="$(dirname $BASH_SOURCE)/bivalvia"
+TEST_PATH="$(dirname $BASH_SOURCE)/test"
 
+# source
+
+echo ${BIVALVIA_PATH}
+for TEST_SCRIPT in $(find ${TEST_PATH} ${SEARCH_PATH} -mindepth 1 -maxdepth 1); do
+    echo "${TEST_SCRIPT}"
+
+    ${TEST_SCRIPT}
 done

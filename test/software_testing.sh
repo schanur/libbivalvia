9f6b2213aa078ef006af8ba435598fd3bd53f54c
Bjoern Griebenow <b.griebenow@web.de>
2017-02-11 16:38:57 +0100

    Add first test case for "software_testing" module

--- test/software_testing.sh
+++ test/software_testing.sh
@@ -1,3 +1,17 @@
-BIVALVIA_PATH="../bivalvia/${0}"
+BIVALVIA_PATH="$(dirname ${BASH_SOURCE})/../bivalvia"
 
-source ${BIVALVIA_PATH}/bivalvia/software_testing.sh
+
+# echo ${BIVALVIA_PATH}
+
+source ${BIVALVIA_PATH}/software_testing.sh
+
+
+function print_abc {
+    echo "abc"
+}
+
+# function expect_2_params {
+#     true
+# }
+
+test_function print_abc 0 "abc" ""

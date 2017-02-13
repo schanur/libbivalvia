dba17b13c95089e071ad41ad6a3695333076336e
Bjoern Griebenow <b.griebenow@web.de>
2017-02-13 07:15:24 +0100

    Fix "csv" module

--- bivalvia/csv.sh
+++ bivalvia/csv.sh
@@ -4,11 +4,11 @@ BIVALVIA_PATH="$(dirname $BASH_SOURCE)"
 # Prints a field in a comma separated list, aka CSV. The first
 # argument is the field index, the second argument is the CSV line.
 function csv_get_field {
-    local FIELD_INDEX=$1
+    local FIELD_INDEX=${1}
+    local FIELD_VALUE
     shift
     local LINE=${*}
-    local FIELD
 
-    FIELD=$(${LINE} | cut -d ',', -f${INDEX})
-    echo ${FIELD}
+    FIELD_VALUE=$(echo ${LINE} | cut -d ',' -f ${FIELD_INDEX})
+    echo ${FIELD_VALUE}
 }

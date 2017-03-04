2001835e01b6ac532c55ec23f107681af9c4e544
Bjoern Griebenow <b.griebenow@web.de>
2017-03-04 03:53:41 +0100

    Add list module

---
+++ test/module/list.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+BIVALVIA_PATH="$(dirname ${BASH_SOURCE})/../../bivalvia"
+
+
+source ${BIVALVIA_PATH}/software_testing.sh
+source ${BIVALVIA_PATH}/list.sh
+
+
+test_function_stdout list_is_empty 0            'a'
+test_function_stdout list_is_empty 0            'a\nb'
+
+test_function_stdout list_add      'a\nb\nc\nd' 'a\nb' 'c\nd'
+
+test_function_stdout list_sub      ''           'a\nb' 'a\nb'
+test_function_stdout list_sub      'b'          'a\nb' 'a'
+test_function_stdout list_sub      'a\nb'       'a\nb' 'c'

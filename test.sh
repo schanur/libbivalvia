4cbc7963a95ce7f3551e3925e83ac363b67be605
Bjoern Griebenow <b.griebenow@web.de>
2018-06-15 23:57:27 +0200

    Show message in test harness if a module script exits != 0

--- test.sh
+++ test.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-BIVALVIA_PATH="   $(dirname "${BASH_SOURCE[0]}")/bivalvia"
+BIVALVIA_PATH="$(dirname "${BASH_SOURCE[0]}")/bivalvia"
 MODULE_TEST_PATH="$(dirname "${BASH_SOURCE[0]}")/test/module"
 
 
+source "${BIVALVIA_PATH}/color.sh"
+
+
 for MODULE_TEST_SCRIPT in $(find "${MODULE_TEST_PATH}" -mindepth 1 -maxdepth 1); do
+    EXIT_CODE=0
     echo "${MODULE_TEST_SCRIPT}"
-    "${MODULE_TEST_SCRIPT}"
+    "${MODULE_TEST_SCRIPT}" || EXIT_CODE=${?}
+    if [ ${EXIT_CODE} -eq 1 ]; then
+	echo 
+	echo "$(with_color red "Test script exited with error code ${EXIT_CODE}: ${MODULE_TEST_SCRIPT}")"
+	echo 
+    fi
 done
--- test/module/args.sh
+++ test/module/args.sh
@@ -3,3 +3,4 @@ set -o errexit -o nounset -o pipefail
 BIVALVIA_PATH="$(dirname "${BASH_SOURCE[0]}")/../../bivalvia"
 
 source "${BIVALVIA_PATH}/software_testing.sh"
+

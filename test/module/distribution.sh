e3df0fda12ec5f69c4a7ca04751758db039b8f42
Bjoern Griebenow <b.griebenow@web.de>
2018-06-15 23:46:26 +0200

    Add tests for distribution name and pretty name

--- test/module/distribution.sh
+++ test/module/distribution.sh
@@ -20,4 +20,30 @@ fedora
 }
 
 
+function os_release_name_known {
+    local OS_RELEASE_ID="${1}"
+    local ID_KNOWN=1
+
+    (echo "
+Debian GNU/Linux
+" | grep -e "^${OS_RELEASE_ID}$") > /dev/null || ID_KNOWN=0
+
+    echo ${ID_KNOWN}
+}
+
+
+function os_release_pretty_name_known {
+    local OS_RELEASE_ID="${1}"
+    local ID_KNOWN=1
+
+    (echo "
+Debian GNU/Linux buster/sid
+" | grep -e "^${OS_RELEASE_ID}$") > /dev/null || ID_KNOWN=0
+
+    echo ${ID_KNOWN}
+}
+
+
 test_function_stdout os_release_id_known          1 "$(distribution_id)"
+test_function_stdout os_release_name_known        1 "$(distribution_name)"
+test_function_stdout os_release_pretty_name_known 1 "$(distribution_pretty_name)"

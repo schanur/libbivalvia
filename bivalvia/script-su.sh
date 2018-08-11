f7584603e896e9d5fa79883de3c0b66ac1a99ae0
Bjoern Griebenow <b.griebenow@web.de>
2018-08-11 16:15:56 +0200

    Fix: run_as_root does not require that binary can be found in $PATH

--- bivalvia/script-su.sh
+++ bivalvia/script-su.sh
@@ -1,12 +1,19 @@
 BIVALVIA_PATH="$(dirname "${BASH_SOURCE[0]}")"
 
+source "${BIVALVIA_PATH}/path.sh"
+
 
 # Runs the script with the same parameters as root.
 function run_as_root {
+    local ABS_BINARY_FILENAME
+
     if [ "${USER}" != "root" ]; then
+        # Absolute path to binary is needed because root user does not
+        # have the same PATH variable.
+        ABS_BINARY_FILENAME=$(with_absolute_path ${0})
         echo "Root privileges are required.
 Please enter root password to continue or CTRL+C to abort."
-        su root -c "echo && ${0} ${*}"
+        su root -l -c "echo && ${ABS_BINARY_FILENAME} ${*}"
         exit
     fi
 }

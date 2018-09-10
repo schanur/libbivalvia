94053bded8e5fab47b91f8de63fd9b5cc184d910
Bjoern Griebenow <b.griebenow@web.de>
2018-09-11 00:45:20 +0200

    Temporary comment sections in git module that do produce errors

--- bivalvia/git.sh
+++ bivalvia/git.sh
@@ -16,29 +16,23 @@ function github_git_prerequirements {
 }
 
 
+# 
 function github_repos_by_user {
     local USERNAME="${1}"
     local CACHE_OBJ_NAME="github_repos_by_user.${USERNAME}"
     local CURL_CMD_STR="curl -s 'https://api.github.com/users/${USERNAME}/repos?page=1&per_page=100'"
     local GITHUB_REPO_LIST_RAW
     local GITHUB_REPO_LIST
-    # local API_RESPONSE_VALID=1
     local EXIT_CODE
 
-    print_var CURL_CMD_STR
+    # print_var CURL_CMD_STR
 
     if [ $(cache__is_available "${CACHE_OBJ_NAME}") -eq 1 ]; then
-        echo "Load cached version"
+        # echo "Load cached version"
         GITHUB_REPO_LIST="$(cache__load ${CACHE_OBJ_NAME})"
     else
-        echo "Load from internet"
-        echo "${CURL_CMD_STR}"
-        echo "$(eval ${CURL_CMD_STR})"
-        # GITHUB_REPO_LIST_RAW=$(${CURL_CMD_STR})
-        # GITHUB_REPO_LIST_RAW="$(curl -s 'https://api.github.com/users/${USERNAME}/repos?page=1&per_page=100')"
-        # GITHUB_REPO_LIST_RAW="$(cat temp)"
-        # echo ${GITHUB_REPO_LIST_RAW}
-        exit 0
+        # echo "Load from internet"
+	GITHUB_REPO_LIST_RAW="$(eval ${CURL_CMD_STR})"
         if [ $(echo "${GITHUB_REPO_LIST_RAW}" | (grep -c '"message": "Not Found",' || true)) -eq 1 ]; then
             # Github API returned "Not found" message.
             critical_error "GitHub API error. Not found: ${GITHUB_REPO_LIST_RAW}"
@@ -49,17 +43,12 @@ function github_repos_by_user {
         GITHUB_REPO_LIST="$(echo "${GITHUB_REPO_LIST_RAW}" \
                           | grep 'html_url' \
                           | grep "${USERNAME}/" \
-                          | sed -e 's|.*'"${USERNAME}"'/||g' \
-                          | sed -e \"s|\',||g\")"
-        echo "${GITHUB_REPO_LIST_RAW}"
-        echo "${GITHUB_REPO_LIST_URL}"
-        # dbg_msg ${GITHUB_REPO_LIST_RAW}
-        # dbg_msg ${GITHUB_REPO_LIST_URL}
-        exit 1
-        if [ ${EXIT_CODE} -eq 0 ]; then
-            echo "Update cache"
-            cache__update "${CACHE_OBJ_NAME}" "${GITHUB_REPO_LIST}"
-        fi
+                          | sed -e 's|.*'"${USERNAME}"'/||g' -e 's|\",||g')"
+	echo "${GITHUB_REPO_LIST}"
+        # if [ ${EXIT_CODE} -eq 0 ]; then
+        #     echo "Update cache"
+        #     cache__update "${CACHE_OBJ_NAME}" "${GITHUB_REPO_LIST}"
+        # fi
     fi
 }
 

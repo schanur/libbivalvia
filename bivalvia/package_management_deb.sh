c37d960c9c1a5c5f95b2ca43332c641dd9234b90
Bjoern Griebenow <b.griebenow@web.de>
2021-09-10 22:03:03 +0200

    feat: add function to check if a package exists in the deb repos

--- bivalvia/package_management_deb.sh
+++ bivalvia/package_management_deb.sh
@@ -1,12 +1,32 @@
 BIVALVIA_PATH="$(dirname "${BASH_SOURCE[0]}")"
 
+source "${BIVALVIA_PATH}/list.sh"
+
+
+GL_PACKAGE_MANAGEMENT_DEB__AVAILABLE_PACKAGE_CACHE=""
+
+
+function deb_package_cache_available_package_list {
+    GL_PACKAGE_MANAGEMENT_DEB__AVAILABLE_PACKAGE_CACHE="$(apt-cache search . | cut -f 1 -d ' ')"
+}
+
+
+function deb_package_available_in_repos {
+    local PACKAGE_NAME="${1}"
+
+    if [ "${GL_PACKAGE_MANAGEMENT_DEB__AVAILABLE_PACKAGE_CACHE}" = "" ]; then
+        deb_package_cache_available_package_list
+    fi
+    list_contains "${GL_PACKAGE_MANAGEMENT_DEB__AVAILABLE_PACKAGE_CACHE}" "${PACKAGE_NAME}"
+}
+
 
 function deb_package_is_installed {
-    local PACKAGE_EXISTS=1
+    local PACKAGE_INSTALLED=1
 
-    dpkg -l "$1" &>/dev/null || PACKAGE_EXISTS=0
+    dpkg -l "$1" &>/dev/null || PACKAGE_INSTALLED=0
 
-    echo ${PACKAGE_EXISTS}
+    echo ${PACKAGE_INSTALLED}
 }
 
 
--- test/module/package_management_deb.sh
+++ test/module/package_management_deb.sh
@@ -3,3 +3,8 @@ set -o errexit -o nounset -o pipefail
 BIVALVIA_PATH="$(dirname "${BASH_SOURCE[0]}")/../../bivalvia"
 
 source "${BIVALVIA_PATH}/software_testing.sh"
+source "${BIVALVIA_PATH}/package_management_deb.sh"
+
+
+test_function_stdout deb_package_available_in_repos 1 'emacs'
+test_function_stdout deb_package_available_in_repos 0 'package-which-does-not-exist'

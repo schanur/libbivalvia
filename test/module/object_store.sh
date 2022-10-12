09ab7c2baf4280ef418f032dcecfae3b88c333ba
Björn Griebenow <b.griebenow@web.de>
2022-10-12 14:07:04 +0200

    refactor: merged object_store

--- test/module/object_store.sh
+++ test/module/object_store.sh
@@ -8,8 +8,8 @@ source "${BIVALVIA_PATH}/object_store.sh"
 
 STORE_NAME="unit_test_object_store"
 
-# test_function_stdout object_store__create_volatile "/dev/shm/$(whoami)/${STORE_NAME}" "${STORE_NAME}"
+test_function_stdout object_store__create_volatile "/dev/shm/$(whoami)/${STORE_NAME}" "${STORE_NAME}"
 
-# test_function_stdout object_store__clear
+test_function_stdout object_store__clear
 
-# test_function_stdout object_store__destroy
+test_function_stdout object_store__destroy
